cd1a0efe0267e066b02fbc31fc888fea00d3475a
wslmacieira <wslmacieira@outlook.com>
2021-06-01 00:01:43 -0300

    challenge completed

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,18 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const user = this.showUserProfileUseCase.execute({ user_id });
+
+      if (!user_id) {
+        throw new Error("User not found");
+      }
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,18 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+
+      if (!user_id) {
+        throw new Error("User not found");
+      }
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
