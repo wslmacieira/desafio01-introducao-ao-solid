a018fc110a2bd71687c0400bbf3560a34eeb6c9a
wslmacieira <wslmacieira@outlook.com>
2021-06-01 20:03:16 -0300

    challenge02 completed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
